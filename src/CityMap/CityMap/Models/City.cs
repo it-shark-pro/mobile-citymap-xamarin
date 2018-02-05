8177a1729575e52eae851d16500db7469a0eea7f
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2018-02-05 11:08:50 +0300

    Add models.

---
+++ src/CityMap/CityMap/Models/City.cs
@@ -0,0 +1,11 @@
+﻿namespace CityMap.Models
+{
+    public class City
+    {
+        public string Name { get; set; }
+
+        public Country Country { get; set; }
+
+        public string Description => $"{Name} is a beautiful city that is located in {Country.Name} country.More information will be available in the future versions of program.Stay tuned!";
+    }
+}
---
+++ src/CityMap/CityMap/Models/Country.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CityMap.Models
+{
+    public class Country
+    {
+        public string Name { get; set; }
+    }
+}
