0a4ce61c070b80dacba8be49e513ac961edf6cc9
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2018-02-05 14:35:15 +0300

    Add cache for API request.

--- src/CityMap/CityMap/Services/CityService.cs
+++ src/CityMap/CityMap/Services/CityService.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Plugin.Connectivity;
+using MonkeyCache.SQLite;
 using CityMap.Models;
 
 namespace CityMap.Services
@@ -11,19 +12,30 @@ namespace CityMap.Services
     public class CityService : ICityService
     {
         private const string ApiUrl = "https://api.myjson.com/bins/7ybe5";
-        private const string NoInternetExceptionMessage = "No internet connection!";
 
         public async Task<IEnumerable<City>> LoadCitiesAsync()
         {
+            // handle online/offline scenario
             if (!CrossConnectivity.Current.IsConnected)
-                throw new Exception(NoInternetExceptionMessage);
+            {
+                return Barrel.Current.Get<IEnumerable<City>>(key: ApiUrl);
+            }
+
+            // handles checking if cache is expired
+            if (!Barrel.Current.IsExpired(key: ApiUrl))
+            {
+                return Barrel.Current.Get<IEnumerable<City>>(key: ApiUrl);
+            }
 
             using (var httpClient = new HttpClient())
             {
                 var jsonResponse = await httpClient.GetStringAsync(ApiUrl);
                 var response = JsonConvert.DeserializeObject<ApiResponse>(jsonResponse);
+                var cities = response.Photos;
 
-                return response.Photos;
+                Barrel.Current.Add(key: ApiUrl, data: cities, expireIn: TimeSpan.FromDays(1));
+
+                return cities;
             }
         }
     }
