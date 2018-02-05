a846622125dfa5b4823cd06f8382f8753a77cb98
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2018-02-05 12:15:15 +0300

    Add Connectivity plugin.

--- src/CityMap/CityMap/Services/CityService.cs
+++ src/CityMap/CityMap/Services/CityService.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Plugin.Connectivity;
 using CityMap.Models;
 
 namespace CityMap.Services
@@ -9,9 +11,13 @@ namespace CityMap.Services
     public class CityService : ICityService
     {
         private const string ApiUrl = "https://api.myjson.com/bins/upt7z";
+        private const string NoInternetExceptionMessage = "No internet connection!";
 
         public async Task<IEnumerable<City>> LoadCitiesAsync()
         {
+            if (!CrossConnectivity.Current.IsConnected)
+                throw new Exception(NoInternetExceptionMessage);
+
             using (var httpClient = new HttpClient())
             {
                 var jsonResponse = await httpClient.GetStringAsync(ApiUrl);
